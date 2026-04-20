c9add7c8da1e3075f02e5ecb49aa82f45dc518d2
kriteshx0x <k.riteshx0x@gmail.com>
2026-04-20 16:01:04 +0530

    refactor: reorganize existing configs

---
+++ zsh/aliases.zsh
@@ -0,0 +1,12 @@
+alias ll='ls -lahF --color=auto'
+alias gs='git status'
+alias glog='git log --oneline'
+alias gp='git push'
+alias ports='ss -tulnp'
+alias openports='ss -tuln'
+alias logs='sudo journalctl -xe'
+alias myip='curl ifconfig.me'
+alias update='sudo apt update && sudo apt upgrade -y'
+alias cls='clear'
+
+
